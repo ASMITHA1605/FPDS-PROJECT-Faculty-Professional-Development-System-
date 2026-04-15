a83e3f79d1eb15777504ca4a59b42a8723b37be9
ASMITHA1605 <hasmitha332@gmail.com>
2026-04-16 00:19:25 +0530

    final deploy

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,23 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  connectTimeout: 10000,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
-    if (err) {
-        console.error("❌ DB ERROR:", err);
-    } else {
-        console.log("✅ DB CONNECTED");
-    }
+  if (err) {
+    console.error("❌ DB ERROR:", err);
+  } else {
+    console.log("✅ DB CONNECTED");
+  }
 });
 
 module.exports = db;
